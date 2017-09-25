e655843d50c4d19f0321d6dbd91a3a0e99f41539
Antonio García <antonio.garcia@arcmedia.ch>
2017-09-25 17:54:15 +0200

    Updated bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
-apt update
-apt install -y apache2
-
-if ! [ -L /var/www ]; then
-  rm -rf /var/www
-  ln -fs /vagrant /var/www
-fi
+###### Prepare and install localhost, php, mysql, etc - more info ---->  http://drupalalsur.org/apuntes/como-instalar-un-servidor-lamp-en-ubuntu-1404 ######
+apt-get -qq update
+apt-get upgrade
+apt-get -y install apache2
+apt-get -y install unzip git
+apt-get -y install apache2 php5 libapache2-mod-php5 php5-mcrypt php5-cli php5-curl php5-intl php5-xsl php-pear php5-xdebug phpunit php5-dev
+apt-get -y install memcached php5-memcached
+pecl install xdebug
